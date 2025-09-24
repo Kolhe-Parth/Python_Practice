74cb7eb7be1ad1d05a048b07846321034c92fc28
Kolhe Parth <parthkolhe77@gmail.com>
2025-09-24 19:48:02 +0530

    Updating Day 7

---
+++ Day_7/Multi_Dimension_Array.py
@@ -0,0 +1,16 @@
+#Multi_Dimension_Array
+
+from numpy import *
+
+arr=array([
+
+      [1,45,33,77,54],
+      [12,43,556,332,233],
+      [23,54,22,43,67]
+
+])
+
+print(arr)
+print("The dimension of array is:",arr.shape)
+
+print("Size of the array is :",arr.size)
