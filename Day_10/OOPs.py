f98061ccc076556af0cd58b6928b129478b68256
Kolhe Parth <parthkolhe77@gmail.com>
2025-09-30 19:26:18 +0530

    Updating Day 10

---
+++ Day_10/OOPs.py
@@ -0,0 +1,14 @@
+#Object-oriented programming in python
+
+class computer:
+    def config(self):
+        print("i5,16 ram,1TB SSD")
+
+
+com1=computer() #creating the object of computer class
+com2=computer()  #creating the second object of computer class
+
+
+com1.config()   #calling the config method using com1 object
+
+com2.config()
