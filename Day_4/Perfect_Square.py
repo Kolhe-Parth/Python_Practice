7a0163dff090991e55a6c47da12d59ea611f4fae
Kolhe Parth <parthkolhe77@gmail.com>
2025-10-08 21:00:19 +0530

    Updating

--- Day_4/Perfect_Square.py
+++
@@ -1,7 +0,0 @@
-# Print the perfect square numbers between 1-500
-
-
-for i in range(1,501):  # 501 because it will go till -1
-     square=i**2
-     if square <= 500:
-         print("Square of",i,"is:",square)
