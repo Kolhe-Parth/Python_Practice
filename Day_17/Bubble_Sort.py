6fa356e97ccbec395186b3f7c7e02fb8fb62983c
Kolhe Parth <parthkolhe77@gmail.com>
2025-10-10 18:56:51 +0530

    Updating Day 17

---
+++ Day_17/Bubble_Sort.py
@@ -0,0 +1,25 @@
+
+list=[]
+
+def sort(list,len):
+    for i in range(len-1,0,-1):
+        for j in range(i):
+            if list[j]>list[j+1]:
+                temp=list[j]
+                list[j]=list[j+1]
+                list[j+1]=temp
+
+size=int(input("Enter the size of data:"))
+
+for i in range(size):
+    data=int(input("Enter the data:"))
+    list.append(data)
+
+
+print("Data before sorting:",list)
+
+sort(list,size)
+print("List after Sorting:",list)
+
+
+
